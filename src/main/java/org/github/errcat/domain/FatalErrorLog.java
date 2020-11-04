bdc7f0ec02cb4e8f28b2a64ae2c6d658a99e4da0
mmillson <you@example.com>
2020-11-04 12:14:58 -0500

    Initial checkin.

---
+++ src/main/java/org/github/errcat/domain/FatalErrorLog.java
@@ -0,0 +1,137 @@
+/**********************************************************************************************************************
+ * errcat                                                                                                             *
+ *                                                                                                                    *
+ * Copyright (c) 2020 Mike Millson                                                                                    *
+ *                                                                                                                    * 
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License       * 
+ * v. 2.0 which is available at https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0 which is    *
+ * available at https://www.apache.org/licenses/LICENSE-2.0.                                                          *
+ *                                                                                                                    *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0                                                                     *
+ *                                                                                                                    *
+ * Contributors:                                                                                                      *
+ *    Mike Millson - initial API and implementation                                                                   *
+ *********************************************************************************************************************/
+
+package org.github.errcat.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.github.errcat.domain.jdk.JvmInfoEvent;
+import org.github.errcat.domain.jdk.OsEvent;
+import org.github.errcat.util.jdk.Analysis;
+
+/**
+ * Fatal error log data.
+ * 
+ * @author <a href="mailto:mmillson@redhat.com">Mike Millson</a>
+ * 
+ */
+public class FatalErrorLog {
+
+    /**
+     * JVM environment information.
+     */
+    private JvmInfoEvent jvminfo;
+
+    /**
+     * OS information.
+     */
+    private OsEvent os;
+
+    /**
+     * Log lines that do not match any existing logging patterns.
+     */
+    private List<String> unidentifiedLogLines;
+
+    /**
+     * Analysis.
+     */
+    private List<Analysis> analysis;
+
+    /*
+     * Default constructor.
+     */
+    public FatalErrorLog() {
+        
+        analysis = new ArrayList<Analysis>();
+        unidentifiedLogLines = new ArrayList<String>();
+    }
+
+    public JvmInfoEvent getJvminfo() {
+        return jvminfo;
+    }
+
+    public void setJvminfo(JvmInfoEvent jvminfo) {
+        this.jvminfo = jvminfo;
+    }
+
+    public OsEvent getOs() {
+        return os;
+    }
+
+    public void setOs(OsEvent os) {
+        this.os = os;
+    }
+
+    public List<String> getUnidentifiedLogLines() {
+        return unidentifiedLogLines;
+    }
+
+    public void setUnidentifiedLogLines(List<String> unidentifiedLogLines) {
+        this.unidentifiedLogLines = unidentifiedLogLines;
+    }
+
+    public List<Analysis> getAnalysis() {
+        return analysis;
+    }
+
+    public void setAnalysis(List<Analysis> analysis) {
+        this.analysis = analysis;
+    }
+
+    /**
+     * Do analysis.
+     */
+    public void doAnalysis() {
+
+        // Unidentified logging lines
+        if (getUnidentifiedLogLines().size() > 0) {
+            analysis.add(0, Analysis.WARN_UNIDENTIFIED_LOG_LINE_REPORT);
+        }
+
+        // TODO:
+        if (haveData()) {
+            doDataAnalysis();
+        }
+    }
+
+    /**
+     * Do data analysis.
+     */
+    private void doDataAnalysis() {
+        // Check for partial log
+        /*
+         * if (this.firstSafepointEvent != null && VmUtil.isPartialLog(firstSafepointEvent.getTimestamp())) {
+         * analysis.add(Analysis.INFO_FIRST_TIMESTAMP_THRESHOLD_EXCEEDED); }
+         */
+    }
+
+    /**
+     * @return true if there is data, false otherwise (e.g. no fatal error log lines recognized).
+     */
+    public boolean haveData() {
+        boolean haveData = true;
+        // TODO:
+        return haveData;
+    }
+
+    /**
+     * Do JVM options analysis.
+     */
+    private void doJvmOptionsAnalysis() {
+
+        // TODO:
+    }
+}
---
+++ src/main/java/org/github/errcat/service/Manager.java
@@ -0,0 +1,97 @@
+/**********************************************************************************************************************
+ * errcat                                                                                                             *
+ *                                                                                                                    *
+ * Copyright (c) 2020 Mike Millson                                                                                    *
+ *                                                                                                                    * 
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License       * 
+ * v. 2.0 which is available at https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0 which is    *
+ * available at https://www.apache.org/licenses/LICENSE-2.0.                                                          *
+ *                                                                                                                    *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0                                                                     *
+ *                                                                                                                    *
+ * Contributors:                                                                                                      *
+ *    Mike Millson - initial API and implementation                                                                   *
+ *********************************************************************************************************************/
+package org.github.errcat.service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.github.errcat.Main;
+import org.github.errcat.domain.FatalErrorLog;
+import org.github.errcat.domain.LogEvent;
+import org.github.errcat.domain.UnknownEvent;
+import org.github.errcat.domain.jdk.JvmInfoEvent;
+import org.github.errcat.domain.jdk.OsEvent;
+import org.github.errcat.util.jdk.JdkUtil;
+
+/**
+ * <p>
+ * Provides fatal error log analysis services to other layers.
+ * </p>
+ * 
+ * @author <a href="mailto:mmillson@redhat.com">Mike Millson</a>
+ * 
+ */
+public class Manager {
+
+    /**
+     * The fatal error log object.
+     */
+    private FatalErrorLog fatalErrorLog;
+
+    /**
+     * Default constructor.
+     */
+    public Manager() {
+        this.fatalErrorLog = new FatalErrorLog();
+    }
+
+    /**
+     * Parse the fatal error log.
+     * 
+     * @param logFile
+     *            The fatal error log file.
+     */
+    public FatalErrorLog parse(File logFile) {
+        if (logFile != null) {
+
+            // Parse vm log file
+            BufferedReader bufferedReader = null;
+            try {
+                bufferedReader = new BufferedReader(new FileReader(logFile));
+                String logLine = bufferedReader.readLine();
+                while (logLine != null) {
+                    LogEvent event = JdkUtil.parseLogLine(logLine);
+                    if (event instanceof JvmInfoEvent) {
+                        fatalErrorLog.setJvminfo((JvmInfoEvent) event);
+                    } else if (event instanceof OsEvent) {
+                        fatalErrorLog.setOs((OsEvent) event);
+                    } else if (event instanceof UnknownEvent) {
+                        if (fatalErrorLog.getUnidentifiedLogLines().size() < Main.REJECT_LIMIT) {
+                            fatalErrorLog.getUnidentifiedLogLines().add(logLine);
+                        }
+                    }
+                    logLine = bufferedReader.readLine();
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                // Close streams
+                if (bufferedReader != null) {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return fatalErrorLog;
+    }
+}
