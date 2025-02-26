8a72f077316355a9a40dd33f09c4fa4bb350da78
mmillson <mgm3746@users.noreply.github.com>
2025-02-26 10:15:44 -0500

    Update copyright date.

--- src/main/java/org/github/krashpad/domain/jdk/LoadAverage.java
+++ src/main/java/org/github/krashpad/domain/jdk/LoadAverage.java
@@ -1,7 +1,7 @@
 /**********************************************************************************************************************
  * krashpad                                                                                                           *
  *                                                                                                                    *
- * Copyright (c) 2020-2024 Mike Millson                                                                               *
+ * Copyright (c) 2020-2025 Mike Millson                                                                               *
  *                                                                                                                    * 
  * This program and the accompanying materials are made available under the terms of the Eclipse Public License       * 
  * v. 2.0 which is available at https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0 which is    *
